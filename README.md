7d7c97259754f4463161c45e02655e92d27af427
Artem Rarog <rarokss228@gmail.com>
2024-06-24 20:24:33 +0700

    first try

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/drippyzxc/mesto-project-ff
--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+     name: "Кёльнский собор",
+     link: "https://i.pinimg.com/564x/09/72/4b/09724be76b8c4854e78d2c42033c9468.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Собор Святого Патрика",
+      link: "https://i.pinimg.com/564x/e3/50/1f/e3501f05df398ee1df7e13ee9cdcbfd8.jpg",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Нотр-Дам-де-Пари",
+      link: "https://gawain.ru/images/paris/8_2_12_notr_dam.jpg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Миланский собор",
+      link: "https://i.pinimg.com/564x/b4/aa/34/b4aa34c80345b503158197eafa6f4068.jpg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Собор Святого Вита",
+      link: "https://i.pinimg.com/564x/56/d8/30/56d830e6659165b92d41f4d5d6f27ea5.jpg",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Саграда Фамилия",
+      link: "https://i.pinimg.com/736x/96/ca/db/96cadb7aca3c3670a44bbdef9620d235.jpg",
     }
 ];
